002178680fdc1c2f9c4ccf64b587a48d148ff846
Franco Josue Matorras <josue487@gmail.com>
2021-08-14 01:10:31 -0300

    Probando kotlin Inicio de app con variables de entorno ToDo base de datos no es un lenguaje complejo

--- app/src/main/java/com/ice/himnarioiglesiacristianaevangelica/BusquedaActivity.kt
+++ app/src/main/java/com/ice/himnarioiglesiacristianaevangelica/BusquedaActivity.kt
@@ -2,13 +2,16 @@ package com.ice.himnarioiglesiacristianaevangelica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ice.himnarioiglesiacristianaevangelica.databinding.ActivityBusquedaBinding
+import com.ice.himnarioiglesiacristianaevangelica.databinding.ActivityMainBinding
 
 class BusquedaActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_busqueda)
-    }
 
+    //Necesario para trabajar con viewbinding
+    private lateinit var binding : ActivityBusquedaBinding
+
+    //Contenido del himno promedio (ToDo Base de datos con estos atributos)
+    var id:Int = 1;
     var titulo:String="A casa vete y cuenta allí"
     var cancion:String="A casa vete y cuenta allí / que Cristo te libró; / que tus amigos vean en ti /" +
             " lo que Él por gracia obró. + A casa vete y cuenta allí / que Cristo comprendió / " +
@@ -16,5 +19,29 @@ class BusquedaActivity : AppCompatActivity() {
             "que Él satisfará / sus almas, puesto que en su amor / la cruz sufrido ha. + Ve, cuenta a " +
             "los de más allá / que en Cristo hay perdón, / y que Él a todos salvará, / si quieren salvación. + "
     var coro : String="A casa vete y lo que en ti / Ha hecho Dios, que vean, / y puede ser que los de allí / lo buscarán también. + "
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //inflar la actividad
+        binding= ActivityBusquedaBinding.inflate(layoutInflater)
+        //seteo del content view
+        setContentView(binding.root)
+        cargarHimno()
+    }
+
+
+
+
+fun cargarHimno(){
+    var tituloBusqueda:String = "$id $titulo";
+    var cancionBusqueda:String = cancion.replace("/", System.getProperty("line.separator"));
+    var coroBuscado:String = coro.replace("/", System.getProperty("line.separator"));
+    binding.lblTitulo.text=tituloBusqueda
+    binding.lblCancion.text=cancionBusqueda
+
+}
+
 
 }
--- app/src/main/java/com/ice/himnarioiglesiacristianaevangelica/MainActivity.kt
+++ app/src/main/java/com/ice/himnarioiglesiacristianaevangelica/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.ice.himnarioiglesiacristianaevangelica
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.ice.himnarioiglesiacristianaevangelica.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //necesario para trabajar con viewbinding
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //inflar la actividad
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        //seteo del content view
+        setContentView(binding.root)
+    }
+
+
+    fun btnBuscar(v: View){
+        if(binding.txtNumeroHimno.text.isNotEmpty()){
+            val intent = Intent(this, BusquedaActivity::class.java)
+            startActivity(intent)
+        }else{
+            Toast.makeText(this, "Debe Ingresar un himno valido", Toast.LENGTH_SHORT).show()
+        }
     }
 }
